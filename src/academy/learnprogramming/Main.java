a5824449dba520f228123d7da3c92a8a1fcba86b
ryoray <praretno.wibowo@gmail.com>
2020-04-20 06:18:07 +0700

    Initial commit

---
+++ src/academy/learnprogramming/Main.java
@@ -0,0 +1,66 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        printDayOfTheWeek(0);
+        printDayOfTheWeek(1);
+        printDayOfTheWeek(2);
+        printDayOfTheWeek(3);
+        printDayOfTheWeek(4);
+        printDayOfTheWeek(5);
+        printDayOfTheWeek(6);
+        printDayOfTheWeek(8);
+        printDayOfTheWeek(-1);
+    }
+
+    public static void printDayOfTheWeek (int day) {
+        String dayName;
+        switch (day) {
+            case 0:
+                dayName = "Sunday";
+                break;
+            case 1:
+                dayName = "Monday";
+                break;
+            case 2:
+                dayName = "Tuesday";
+                break;
+            case 3:
+                dayName = "Wednesday";
+                break;
+            case 4:
+                dayName = "Thursday";
+                break;
+            case 5:
+                dayName = "Friday";
+                break;
+            case 6:
+                dayName = "Saturday";
+                break;
+            default:
+                dayName = "Invalid day";
+                break;
+        }
+        System.out.println(dayName);
+
+        if (day == 0) {
+            System.out.println("Sunday");
+        } else if (day == 1) {
+            System.out.println("Monday");
+        } else if (day == 2) {
+            System.out.println("Tuesday");
+        } else if (day == 3) {
+            System.out.println("Wednesday");
+        } else if (day == 4) {
+            System.out.println("Thursday");
+        } else if (day == 5) {
+            System.out.println("Friday");
+        } else if (day == 6){
+            System.out.println("Saturday");
+        } else {
+            System.out.println("Invalid day");
+        }
+    }
+}
